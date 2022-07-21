5687bcf0ff4a0877ab349d9f782ae229731cad7d
Federico Lombardi <federicolombardi_10@hotmail.com>
2022-07-21 12:38:27 -0300

    hero

--- src/components/MyHero.js
+++ src/components/MyHero.js
@@ -1,15 +1,15 @@
+import Button from "react-bootstrap/Button"
 import { Link } from "react-router-dom"
 const MyHero=()=>{
     return(
         <>
-        <div class="p-5 overflow-hidden d-flex flex-column align-items-center justify-content-center text-center border-bottom bg min-vh-100">
-            <h1 class="display-4 fw-bold">SOMOS LO QUE HACEMOS</h1>
-            <div class="col-lg-6 mx-auto">
-                <p class="lead mb-4">Pero sobre todo, somos lo que hacemos para cambiar lo que somos...</p>
-                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-                    {/* <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3">Primary button</button> */}
+        <div className="p-5 overflow-hidden d-flex flex-column align-items-center justify-content-center text-center border-bottom bg min-vh-100">
+            <h1 className="display-4 fw-bold">SOMOS LO QUE HACEMOS</h1>
+            <div className="col-lg-6 mx-auto">
+                <p className="lead mb-4">Pero sobre todo, somos lo que hacemos para cambiar lo que somos...</p>
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
                     <Link to={'/productos'}>
-                        <button type="button" class="btn btn-warning btn-lg px-4">Comprar ahora</button>
+                        <Button type="button" className="btn btn-warning btn-lg px-4">Comprar ahora</Button>
                     </Link>
                 </div>
             </div>
