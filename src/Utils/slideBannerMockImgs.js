f0e97faafce1293a9c2f4c077764e404beae58ef
Federico Lombardi <federicolombardi_10@hotmail.com>
2022-09-08 10:47:41 -0300

    prueba

--- src/Utils/slideBannerMockImgs.js
+++ src/Utils/slideBannerMockImgs.js
@@ -1,6 +1,6 @@
 const slideBannerImgs=[
 {
-image:"/tienda.jpg",
+image:"/3.jpg",
 info:"First slide",
 titleImg:"First slide label",
 textImg:"Nulla vitae elit libero, a pharetra augue mollis interdum."    
